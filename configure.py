d77eafcae5df17c33197913c06ff809effe3fde6
Johan Euphrosine <proppy@google.com>
2022-12-03 03:03:10 +0900

    configure: remove top restriction

--- configure.py
+++ configure.py
@@ -178,6 +178,5 @@ if __name__ == '__main__':
         config = load_yaml(args.yaml)
         source_files = get_project_source(config)
         top_module = get_top_module(config)
-        assert top_module != 'top'
         io_ranges = get_io_ranges(config)
         write_user_config(top_module, source_files, io_ranges)
